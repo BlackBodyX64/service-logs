ccc4eeda9669913cb1429f7a40b5f0174bf405bd
Anon Thamcharoen <anonnon3952@gmail.com>
2024-07-04 23:55:19 +0700

    disable gaud

--- src/routes.ts
+++ src/routes.ts
@@ -3,9 +3,9 @@ import * as LogController from './controllers/LogController'
 
 const router = Router()
 
-router.all('*', (_: Request, res: Response) => {
-    return res.send(`You have no permission to access this content. Service version: 1.0.0`)
-})
+// router.all('*', (_: Request, res: Response) => {
+//     return res.send(`You have no permission to access this content. Service version: 1.0.0`)
+// })
 
 router.post('/sync-log', LogController.syncUpdateLog)
 
