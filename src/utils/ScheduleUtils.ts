2b023db28702e4f63b0f1a19d0fd84ed474c50b4
Anon Thamcharoen <anonnon3952@gmail.com>
2024-07-04 23:38:28 +0700

    40

--- src/utils/ScheduleUtils.ts
+++ src/utils/ScheduleUtils.ts
@@ -4,7 +4,7 @@ import { updateLog, autoPushToKlock } from '../controllers/LogController'
 export default class Schedule {
 
     public static async cronStart () {
-        new cronJob('35 * * * *', updateLog).start()
+        new cronJob('40 * * * *', updateLog).start()
         // new cronJob('15 * * * *', autoPushToKlock).start()
     }
 }
